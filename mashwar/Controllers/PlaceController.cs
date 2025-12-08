cf25a822d1a3a08bc7c333228f41589add9249d9
SUBHI888 <abu3obaida2002@gmail.com>
2025-12-08 11:44:50 -0800

    mm

--- mashwar/Controllers/PlaceController.cs
+++ mashwar/Controllers/PlaceController.cs
@@ -26,7 +26,7 @@ namespace mashwar.Controllers
         {
             ViewBag.App = new SelectList(_Db.Users, "User_Id", "User_Name");
             return View();
-            //d
+            //edit
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
